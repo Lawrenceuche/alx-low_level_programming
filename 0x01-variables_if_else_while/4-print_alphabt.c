ac0a909961ec36df4eec9e8a645f5e413f884c4e
“Lawrenceuche” <dlawamaifeobu16@gmail.com>
2022-09-11 20:20:49 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,14 +3,17 @@
  * main - program that prints all the lowercase in an alphabet
  * followed by a new line 
  * expect q and e
- * */
+ * Return: 0
+ */
 int main(void)
 {
 	char ch = 'a';
-	for (ch ='a'; ch <='z'; ch++);
-	!((ch = 'q')&&(ch ='e'));
+	for (ch ='a'; ch <='z'; ch++)
 	{
-		putchar(ch);
+		if (ch !='q' && ch !='e')
+		{
+			putchar(ch);
+		}
 	}
 	putchar('\n');
 	return (0);
