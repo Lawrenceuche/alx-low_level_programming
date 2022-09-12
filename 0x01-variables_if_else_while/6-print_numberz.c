f41ea48b9c16cf37579cd75b8a2545ca56f90260
“Lawrenceuche” <dlawamaifeobu16@gmail.com>
2022-09-12 05:36:33 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,12 @@
  * main - program that prints all single digit numbers of base 10 
  * starting from 0, followed by a new line.
  * Return : 0
- */
+*/
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
