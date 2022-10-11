20e564c16c6acba993e847d82519a4bb748b3e00
“Lawrenceuche” <dlawamaifeobu16@gmail.com>
2022-10-10 18:51:58 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#ifndef ABS(x)
-#define ABS(x)
-/**
-* main - prints absolute value
-* Return: 0
-*/
-int main(void)
-{
-#if (ABS < 0)
-	(x) * (-1);
-#else(x)
-}
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
 #endif
